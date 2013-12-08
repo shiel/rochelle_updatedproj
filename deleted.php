874ed75b8da110979d38a93774c6f0e8fc826f6f
rochelle <shielmtongol@yahoo.com>
2013-12-08 08:23:01 +0800

    Create deleted.php

---
+++ deleted.php
@@ -0,0 +1,8 @@
+<?php
+include 'config.php';
+$_name = $_POST['name'];
+$_email = $_POST['email'];
+$delete = MessageDAO::deleteMessage($_name,$_email);
+	echo "<center><h2>";
+	echo "<script>alert('Deleted Successfully.');window.location.href='sentItems.php';</script>";
+?>
